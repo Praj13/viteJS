fdba609b62e91d04a910b80434339728c17c66a5
Praj13 <rajbansiprajwol@gmail.com>
2024-07-02 21:33:35 +0545

    useEffect

---
+++ useEffect/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = (props) => {
+   
+  return (
+    <div>
+      <nav>
+        Its a nav bar of color {props.color}
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
